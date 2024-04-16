04b4a7f3d26db685e7ce35296f828de93774bb3e
MkhitaryanMartin <1992Martin@mail.ru>
2024-04-16 17:15:58 +0400

    blog page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,6 +1,6 @@
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { auth, firestore } from '../../firebase';
-<<<<<<< HEAD
+
 import { useAuthState } from "react-firebase-hooks/auth"
 import "./style.css"
 import CommentForm from './comment-form';
@@ -67,53 +67,38 @@ export default function Blog() {
         await blogRef.update({ comments: updatedComments });
     }
 console.log(user)
-    return (
-        <section>
-            <button onClick={createBlog}>createBlog</button>
-            {
-                values && values.map((value,i) => {
-                   return <div key={value.id || i} className='blog'>
-                        <div className='blog-text' >
-                            <h3>{value.blogTitle}</h3>
-                            <p>{value.blogText}</p>
-                        </div>
-                        <div className='blog-comments'>
-                                {value.comments.map((comment,i)=>{
-                                    return <div key={i} className='comment' onClick={(e)=> {
-                                        e.stopPropagation();
-                                        setIndex({blogId:value.id, commentI:i})
-                                    }}>
-                                        <p>{comment.text}</p>
-                                       {index.commentI === i && value.id === index.blogId ?  <CommentForm 
-                                        handleSubmit={answerComment} 
-                                        buttonText='Answer comment' 
-                                        placeholder='answer comment'
-                                        params={{blogId:value?.id, uid: user?.uid, parentId: comment?.id}}
-                                        />:null}
-                                    </div>
-                                })}
+return(
+   <section>
+        <button onClick={createBlog}>createBlog</button>
+    {
+        values && values.map((value,i) => {
+           return <div key={value.id || i} className='blog'>
+                <div className='blog-text' >
+                    <h3>{value.blogTitle}</h3>
+                    <p>{value.blogText}</p>
+                </div>
+                <div className='blog-comments'>
+                        {value.comments.map((comment,i)=>{
+                            return <div key={i} className='comment' onClick={(e)=> {
+                                e.stopPropagation();
+                                setIndex({blogId:value.id, commentI:i})
+                            }}>
+                                <p>{comment.text}</p>
+                               {index.commentI === i && value.id === index.blogId ?  <CommentForm 
+                                handleSubmit={answerComment} 
+                                buttonText='Answer comment' 
+                                placeholder='answer comment'
+                                params={{blogId:value?.id, uid: user?.uid, parentId: comment?.id}}
+                                />:null}
                             </div>
-                   
-                            <CommentForm handleSubmit={addCommentToBlog} buttonText="Create Comment" params={{blogId:value?.id}}/>
+                        })}
                     </div>
-                })
-            }
-=======
-
-export default function Blog(){
-    const [values, loading, error] = useCollectionData(
-        firestore.collection('comments'));
-console.log(values)
-    const sendMessage =async ()=>{
-        firestore.collection("comments").add({
-            coment:"asdasd",
-            id:Math.random()
+           
+                    <CommentForm handleSubmit={addCommentToBlog} buttonText="Create Comment" params={{blogId:value?.id}}/>
+            </div>
         })
     }
-    return (
-        <section>
-<button onClick={sendMessage}>send</button>
->>>>>>> 0a6632ee47082383f9db8c8bc7a784ab1857a076
-        </section>
-    )
+   </section>
+)
 }
+
