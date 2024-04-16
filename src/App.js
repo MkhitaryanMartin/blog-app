998d6ba4c3e88a19c3387fc745c47a9fced157be
MkhitaryanMartin <1992Martin@mail.ru>
2024-04-16 10:58:03 +0400

    Blog comp add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './component/NavBar/NavBar';
+import Blog from './pages/blog';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Blog/>
     </div>
   );
 }
