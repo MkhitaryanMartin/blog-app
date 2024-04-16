7fb2bdb1690ff7b36672a8c8abccaded623c9de2
ArturKocharyan <a.kocharyan1997@gmail.com>
2024-04-16 16:26:25 +0400

    loader

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -4,12 +4,12 @@ import SignIn from './SignIn/SignIn'
 import RegistModal from './RegistModal/RegistModal'
 import { onAuthStateChanged, signOut } from 'firebase/auth'
 import { auth } from '../../firebase'
-import { Button } from 'antd'
+import { Button, Spin } from 'antd' 
 
 export default function NavBar() {
-
     const [authUser, setAuthUser] = useState(null)
     const [userName, setUserName] = useState('')
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const listen = onAuthStateChanged(auth, (user) => {
@@ -19,33 +19,37 @@ export default function NavBar() {
             } else {
                 setAuthUser(null)
             }
+            setLoading(false)
         })
         return () => {
             listen();
         };
-    }, [])
+    }, [userName])
 
     function userSignOut() {
+        setLoading(true) 
         signOut(auth)
             .then(() => console.log("success"))
-            .catch((e) => console.log(e));
+            .catch((e) => console.log(e))
+            .finally(() => setLoading(false));
     }
 
-
     return (
-        <nav className={styles.main_container} >
-            <div className={styles.nav_container} >
-                <div className={styles.logo_container} >
-                    {authUser ? (
+        <nav className={styles.main_container}>
+            <div className={styles.nav_container}>
+                <div className={styles.logo_container}>
+                    {loading ? (
+                        <Spin size="large" />
+                    ) : authUser ? (
                         <div><p>{` ${userName + '` '}s blog `}</p></div>
                     ) : "Dont sign in yet"}
                 </div>
-                {authUser ? (
+                {loading ? null : authUser ? (
                     <div>
                         <Button onClick={userSignOut}>Sign Out</Button>
                     </div>
                 ) : (
-                    <div className={styles.log_container} >
+                    <div className={styles.log_container}>
                         <RegistModal />
                         <SignIn />
                     </div>
