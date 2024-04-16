e9e30828783f3c97feb0c6cb9d099854c56e4f05
MkhitaryanMartin <1992Martin@mail.ru>
2024-04-16 11:20:56 +0400

    hooks

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,8 +2,8 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { auth, firestore } from '../../firebase';
 
 export default function Blog(){
-    // const [values, loading, error] = useCollectionData(
-    //     firestore.collection('messages'));
+    const [values, loading, error] = useCollectionData(
+        firestore.collection('comments'));
 
     const sendMessage =async ()=>{
         firestore.collection("comments").add({
