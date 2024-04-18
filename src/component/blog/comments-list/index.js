3d7fb71b6ef59ef91ab99ec5aef93da0f752ee1f
MkhitaryanMartin <1992Martin@mail.ru>
2024-04-18 21:53:15 +0400

    styles fix

---
+++ src/component/blog/comments-list/index.js
@@ -0,0 +1,151 @@
+import { firestore } from '../../../firebase';
+import CommentForm from '../../comment-form';
+import { useState } from 'react';
+import { DeleteOutlined, EditOutlined, CommentOutlined } from '@ant-design/icons';
+import { Tooltip } from 'antd';
+import { getDayText } from '../../../utilits/getData';
+import { v4 as uuidv4 } from 'uuid';
+import styles from '../../../pages/blog/styles.module.css'
+
+
+export default function CommentList({ value, user }) {
+    const [index, setIndex] = useState("");
+    const [openEdte, setOpenEdite] = useState(false);
+    const [openAnswer, setOpenAnswer] = useState(false);
+    const [parentId, setParentId] = useState("");
+
+
+    const answerComment = async (params) => {
+        if (user) {
+            try {
+                const blogRef = firestore.collection("blogs").doc(params.blogId);
+                const blogSnapshot = await blogRef.get();
+                const currentComments = blogSnapshot.data().comments || [];
+                const updatedComments = [...currentComments, { ...params, id: uuidv4(), createdAt: new Date() }];
+                await blogRef.update({ comments: updatedComments });
+                console.log("New comment added to blog with ID:", params.blogId);
+            } catch (error) {
+                console.error("Error adding comment:", error);
+            }
+        }
+        setIndex({})
+    }
+
+    const deleteComment = async (blogId, commentId) => {
+        try {
+            const blogRef = firestore.collection("blogs").doc(blogId);
+            const blogSnapshot = await blogRef.get();
+            const currentComments = blogSnapshot.data().comments || [];
+            const updatedComments = currentComments.filter(comment => comment.id !== commentId);
+            await blogRef.update({ comments: updatedComments });
+
+            console.log("Comment deleted successfully:", commentId);
+        } catch (error) {
+            console.error("Error deleting comment:", error);
+        }
+    }
+
+    const editComment = async (params) => {
+        try {
+            const blogRef = firestore.collection("blogs").doc(params.blogId);
+            const blogSnapshot = await blogRef.get();
+            const currentComments = blogSnapshot.data().comments || [];
+
+            const updatedComments = currentComments.map(comment => {
+                if (comment.id === params.commentId) {
+                    return { ...comment, text: params.text };
+                }
+                return comment;
+            });
+
+            await blogRef.update({ comments: updatedComments });
+
+            console.log("Comment edited successfully:", params.commentId);
+        } catch (error) {
+            console.error("Error editing comment:", error);
+        }
+        setOpenEdite(false)
+    }
+
+
+ return (
+        <div className={styles.blog_comments}>
+            {value.comments.map((comment, i) => {
+                return <div key={i} 
+                className={`${styles.comment} ${parentId === comment.id ? styles.active_comment : ""}
+                 ${comment?.uid === user?.uid ? styles.user_comment : ""}`} id={comment.id}>
+                    <div className={styles.comment_text_block}>
+                        <p className={styles.comment_date}>{getDayText(comment.createdAt)}</p>
+                        <p className={styles.comment_userName}>{comment.userName}</p>
+                        <Tooltip title="comment that was replied to">
+                            {comment.parentId ? <a onClick={(e) => {
+                                e.stopPropagation()
+                                setParentId(comment.parentId)
+                                setTimeout(() => {
+                                    setParentId("")
+                                }, 7000)
+                            }} className={styles.parent_comment} href={`#${comment.parentId}`}>{comment?.parentComment}</a> : null}
+                        </Tooltip>
+                        <p>{comment.text}</p>
+                        <div className={styles.comment_text_icon_block}>
+
+                            {user?.uid === comment.uid ? <>
+                                <Tooltip title="Delete">
+                                    <DeleteOutlined className={styles.comment_delet_icon} onClick={(e) => {
+                                        e.stopPropagation()
+                                        deleteComment(value.id, comment.id)
+
+                                    }} />
+                                </Tooltip>
+                                <Tooltip title="Edite">
+                                    <EditOutlined className={styles.comment_edite_icon} onClick={(e) => {
+                                        e.stopPropagation()
+                                        setIndex({ commentI: i, blogId: value.id })
+                                        setOpenAnswer(false)
+                                        if (index.blogId === value.id && index.commentI === i) {
+                                            setOpenEdite(!openEdte)
+                                        } else {
+                                            setOpenEdite(true)
+                                        }
+
+                                    }} />
+                                </Tooltip>
+
+                            </> : null}
+                            {user ? <Tooltip title="Answer">
+                                <CommentOutlined
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        setIndex({ blogId: value.id, commentI: i })
+                                        setOpenEdite(false)
+                                        if (index.blogId === value.id && index.commentI === i) {
+                                            setOpenAnswer(!openAnswer)
+                                        } else {
+                                            setOpenAnswer(true)
+                                        }
+                                    }} />
+                            </Tooltip> : null}
+                        </div>
+                    </div>
+                    {index?.commentI === i && value?.id === index?.blogId && !openEdte && openAnswer ? <CommentForm
+                        mode='answer-form'
+                        handleSubmit={answerComment}
+                        buttonText='Answer comment'
+                        placeholder='Answer comment'
+                        params={{ blogId: value?.id, uid: user?.uid, parentId: comment?.id, userName: user?.displayName, parentComment: comment?.text.substring(0, 10) }}
+                    /> : openEdte && !openAnswer && i === index.commentI && value.id === index.blogId ? <CommentForm
+                        mode='answer-form'
+                        params={{ blogId: value.id, commentId: comment.id }}
+                        handleSubmit={editComment}
+                        placeholder="Edite comment"
+                        buttonText='Edite comment'
+                    /> : null}
+                </div>
+            })}
+        </div>
+
+
+    )
+}
+
+
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,67 +1,37 @@
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { auth, firestore } from '../../firebase';
-import CommentForm from './comment-form';
+import CommentForm from '../../component/comment-form';
 import { useEffect, useState } from 'react';
 import { useAuthState } from "react-firebase-hooks/auth";
-import { DeleteOutlined, EditOutlined, CommentOutlined } from '@ant-design/icons';
-import { Button, Tooltip } from 'antd';
+import { Button } from 'antd';
 import AddBlog from '../../component/AddBlog/AddBlog';
 import CreatedBlog from '../../component/CreatedBlog/CreatedBlog';
-import { getDayText } from '../../utilits/getData';
 import SignIn from '../../component/NavBar/SignIn/SignIn';
 import { v4 as uuidv4 } from 'uuid';
 import firebase from 'firebase/compat/app';
 import styles from './styles.module.css'
-import Input from '../../component/Input/input';
 import BlogFilter from '../../component/blog/blog-filter';
 import { FaPenFancy } from "react-icons/fa6";
-import { IoClose } from "react-icons/io5";
+import CommentList from '../../component/blog/comments-list';
+import { customFilter } from './utilits';
 
 
 
 export default function Blog() {
     const [user] = useAuthState(auth)
-    const [values, loading, error] = useCollectionData(
-        firestore.collection('blogs').orderBy("createdAt", "desc")
-    );
-    const [index, setIndex] = useState("");
-    const [openEdte, setOpenEdite] = useState(false);
-    const [openAnswer, setOpenAnswer] = useState(false);
-    const [parentId, setParentId] = useState("");
-    const [searchBlog, setSearchBlog] = useState("")
-    const [showBlog, setShowBlog] = useState(false)
+    const [values, loading, error] = useCollectionData(firestore.collection('blogs').orderBy("createdAt", "desc"));
+    const [searchBlog, setSearchBlog] = useState("");
+    const [showBlog, setShowBlog] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(null);
 
 
-
-    const answerComment = async (params) => {
-        if (user) {
-            try {
-                const blogRef = firestore.collection("blogs").doc(params.blogId);
-                const blogSnapshot = await blogRef.get();
-                const currentComments = blogSnapshot.data().comments || [];
-                const updatedComments = [...currentComments, { ...params, id: uuidv4(), createdAt: new Date() }];
-                await blogRef.update({ comments: updatedComments });
-                console.log("New comment added to blog with ID:", params.blogId);
-            } catch (error) {
-                console.error("Error adding comment:", error);
-            }
+    useEffect(() => {
+        if (!user) {
+            setSelectedOption(null)
+            setSearchBlog("")
         }
-        setIndex({})
-    }
+    }, [user])
 
-    const deleteComment = async (blogId, commentId) => {
-        try {
-            const blogRef = firestore.collection("blogs").doc(blogId);
-            const blogSnapshot = await blogRef.get();
-            const currentComments = blogSnapshot.data().comments || [];
-            const updatedComments = currentComments.filter(comment => comment.id !== commentId);
-            await blogRef.update({ comments: updatedComments });
-
-            console.log("Comment deleted successfully:", commentId);
-        } catch (error) {
-            console.error("Error deleting comment:", error);
-        }
-    }
 
     const deleteBlog = async (blogId) => {
         try {
@@ -72,29 +42,6 @@ export default function Blog() {
         } catch (error) {
             console.error("Error deleting blog:", error);
         }
-    }
-
-
-    const editComment = async (params) => {
-        try {
-            const blogRef = firestore.collection("blogs").doc(params.blogId);
-            const blogSnapshot = await blogRef.get();
-            const currentComments = blogSnapshot.data().comments || [];
-
-            const updatedComments = currentComments.map(comment => {
-                if (comment.id === params.commentId) {
-                    return { ...comment, text: params.text };
-                }
-                return comment;
-            });
-
-            await blogRef.update({ comments: updatedComments });
-
-            console.log("Comment edited successfully:", params.commentId);
-        } catch (error) {
-            console.error("Error editing comment:", error);
-        }
-        setOpenEdite(false)
     }
 
     const editBlog = async (params) => {
@@ -139,115 +86,47 @@ export default function Blog() {
     const onChanSearch = (newValue) => {
         setSearchBlog(newValue);
     }
-    const [selectedOption, setSelectedOption] = useState(null);
+
 
     const handleOptionChange = (option) => {
         setSelectedOption(option);
     };
 
-    useEffect(() => {
-        if (!user) {
-            setSelectedOption(null)
-            setSearchBlog("")
-        }
-    }, [user])
+
     return (
         <section>
             <div className={styles.creator_blog} >
                 {showBlog === true ? <AddBlog onSubmit={createBlog} submitText='Create blog' /> : ''}
                 <span className={styles.blog_button_container} >
                     {user ? (<Button onClick={() => { showBlog === false ? setShowBlog(true) : setShowBlog(false) }} >
-                    {showBlog === false ? <span>Create Blog ... <FaPenFancy /></span> : "Close Blog Editor"}
+                        {showBlog === false ? <span>Create Blog ... <FaPenFancy /></span> : "Close Blog Editor"}
                     </Button>) : ''}
                 </span>
             </div>
-            <BlogFilter value={searchBlog} onChange={onChanSearch} onChangeRadio={handleOptionChange} valiuRadio={selectedOption} user={user} />
+            <BlogFilter
+                value={searchBlog}
+                onChange={onChanSearch}
+                onChangeRadio={handleOptionChange}
+                valiuRadio={selectedOption}
+                user={user}
+            />
             {
-                values && values.filter((value) => {
-                    if (selectedOption && user) {
-                        return value?.blogTitle.toLowerCase().includes(searchBlog.toLowerCase()) && user?.uid === value.uid
-                    } else {
-                        console.log("else", selectedOption)
-                        return value?.blogTitle.toLowerCase().includes(searchBlog.toLowerCase())
-                    }
-                }
-                ).map((value, i) => {
+                values && customFilter(values, selectedOption, user, searchBlog).map((value, i) => {
                     return <div key={value.id || i} className={styles.blog}>
-                        <CreatedBlog blog={value} deleteBlog={deleteBlog} editBlog={editBlog} user={user} />
-                        <div className={styles.blog_comments}>
-                            {value.comments.map((comment, i) => {
-                                return <div key={i} className={`comment ${parentId === comment.id ? styles.active_comment : ""} ${comment?.uid === user?.uid ? styles.user_comment : ""}`} id={comment.id}>
-                                    <div className={styles.comment_text_block}>
-                                        <p className={styles.comment_date}>{getDayText(comment.createdAt)}</p>
-                                        <p className={styles.comment_userName}>{comment.userName}</p>
-                                        <Tooltip title="comment that was replied to">
-                                            {comment.parentId ? <a onClick={(e) => {
-                                                e.stopPropagation()
-                                                setParentId(comment.parentId)
-                                                setTimeout(() => {
-                                                    setParentId("")
-                                                }, 7000)
-                                            }} className={styles.parent_comment} href={`#${comment.parentId}`}>{comment?.parentComment}</a> : null}
-                                        </Tooltip>
-                                        <p>{comment.text}</p>
-                                        <div className={styles.comment_text_icon_block}>
+                        <CreatedBlog
+                            blog={value}
+                            deleteBlog={deleteBlog}
+                            editBlog={editBlog}
+                            user={user} />
+                        <CommentList
+                            user={user}
+                            value={value}
+                        />
 
-                                            {user?.uid === comment.uid ? <>
-                                                <Tooltip title="Delete">
-                                                    <DeleteOutlined className={styles.comment_delet_icon} onClick={(e) => {
-                                                        e.stopPropagation()
-                                                        deleteComment(value.id, comment.id)
-
-                                                    }} />
-                                                </Tooltip>
-                                                <Tooltip title="Edite">
-                                                    <EditOutlined className={styles.comment_edite_icon} onClick={(e) => {
-                                                        e.stopPropagation()
-                                                        setIndex({ commentI: i, blogId: value.id })
-                                                        setOpenAnswer(false)
-                                                        if (index.blogId === value.id && index.commentI === i) {
-                                                            setOpenEdite(!openEdte)
-                                                        } else {
-                                                            setOpenEdite(true)
-                                                        }
-
-                                                    }} />
-                                                </Tooltip>
-
-                                            </> : null}
-                                            {user ? <Tooltip title="Answer">
-                                                <CommentOutlined
-                                                    onClick={(e) => {
-                                                        e.stopPropagation();
-                                                        setIndex({ blogId: value.id, commentI: i })
-                                                        setOpenEdite(false)
-                                                        if (index.blogId === value.id && index.commentI === i) {
-                                                            setOpenAnswer(!openAnswer)
-                                                        } else {
-                                                            setOpenAnswer(true)
-                                                        }
-                                                    }} />
-                                            </Tooltip> : null}
-                                        </div>
-                                    </div>
-                                    {index?.commentI === i && value?.id === index?.blogId && !openEdte && openAnswer ? <CommentForm
-                                        mode='answer-form'
-                                        handleSubmit={answerComment}
-                                        buttonText='Answer comment'
-                                        placeholder='Answer comment'
-                                        params={{ blogId: value?.id, uid: user?.uid, parentId: comment?.id, userName: user?.displayName, parentComment: comment?.text.substring(0, 10) }}
-                                    /> : openEdte && !openAnswer && i === index.commentI && value.id === index.blogId ? <CommentForm
-                                        mode='answer-form'
-                                        params={{ blogId: value.id, commentId: comment.id }}
-                                        handleSubmit={editComment}
-                                        placeholder="Edite comment"
-                                        buttonText='Edite comment'
-                                    /> : null}
-                                </div>
-                            })}
-                        </div>
-
-                        {user ? <CommentForm handleSubmit={addCommentToBlog} buttonText="Create Comment" params={{ blogId: value?.id, uid: user?.uid, userName: user?.displayName }} /> : (
+                        {user ? <CommentForm
+                            handleSubmit={addCommentToBlog}
+                            buttonText="Create Comment"
+                            params={{ blogId: value?.id, uid: user?.uid, userName: user?.displayName }} /> : (
                             <div className={styles.comment_signin}>
                                 <p>Login to write a comment</p>
                                 <SignIn />
